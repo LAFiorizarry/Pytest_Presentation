d623226179e8a206fe68a9301afbdcdd77605e82
Laura Fiorentino <1515930783@mil>
2022-02-28 21:42:07 -0500

    stuff

--- test_ssp_fxns.py
+++ test_ssp_fxns.py
@@ -5,8 +5,9 @@ Created on Fri Feb 25 12:19:39 2022
 @author: Laura.Fiorentino
 """
 
+import csv
 import pytest
-from ssp_fxns import read_ssp
+from ssp_fxns import read_ssp, write_csv
 
 
 @pytest.fixture
@@ -15,12 +16,17 @@ def example_ssp_dict():
     return ssp_dict
 
 
-def test_openfile(example_ssp_dict):
-    test_keys = ['AVG_time', 'AVG', 'AVGSDI_time', 'AVGSDI', 'STD_time',
-                 'STD', 'STDSDI_time', 'STDSDI', 'CNT_time', 'CNT',
-                 'CNTSDI_time', 'CNTSDI', 'OUT_time', 'OUT', 'OUTSDI_time',
-                 'OUTSDI']
-    assert list(example_ssp_dict.keys()) == test_keys
+@pytest.fixture
+def example_keys():
+    example_keys = ['AVG_time', 'AVG', 'AVGSDI_time', 'AVGSDI', 'STD_time',
+                    'STD', 'STDSDI_time', 'STDSDI', 'CNT_time', 'CNT',
+                    'CNTSDI_time', 'CNTSDI', 'OUT_time', 'OUT', 'OUTSDI_time',
+                    'OUTSDI']
+    return example_keys
+
+
+def test_openfile(example_ssp_dict, example_keys):
+    assert list(example_ssp_dict.keys()) == example_keys
 
 
 def test_bad(example_ssp_dict):
@@ -35,4 +41,14 @@ def test_timeformat(example_ssp_dict):
     assert example_ssp_dict['AVG_time'][0] == '11/29/2021 14:55:30'
 
 
-def test_writecsv(example_ssp_dict)
+def test_writecsv(example_ssp_dict, example_keys, tmpdir):
+    filename = tmpdir.join("temp_csv")
+    write_csv(example_ssp_dict, filename)
+    with open(filename) as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        r1 = next(csv_reader)
+        r2 = next(csv_reader)
+        assert r1 == example_keys
+        assert r2[0] == '11/29/2021 14:55:30'
+        assert r2[1] == 'NaN'
+        assert r2[9] == '0'
